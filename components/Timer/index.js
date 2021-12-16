1e629ac764bed1858f1865aa2db669d6903f2f00
Honey <yurjune@naver.com>
2021-12-17 00:07:56 +0900

    웹소켓으로 시간 가져오기

--- components/Timer/index.js
+++ components/Timer/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlayCircle, faStopCircle } from '@fortawesome/free-regular-svg-icons';
+import { faPlayCircle, faStopCircle, faPauseCircle } from '@fortawesome/free-regular-svg-icons';
 import {
   Wrapper,
   SmallWrapper,
@@ -17,23 +17,29 @@ const convertTime = (time) => {
   return time;
 };
 
-export const Timer = ({ hours, minutes, seconds, operateTimer }) => {
+export const Timer = ({ me, hours, minutes, seconds, isStop, operateTimer }) => {
   return (
     <Wrapper>
-      <Time>
-        <Count>{convertTime(hours)}</Count>
-        <Count>:</Count>
-        <Count>{convertTime(minutes)}</Count>
-        <Count>:</Count>
-        <Count>{convertTime(seconds)}</Count>
-      </Time>
-      <FontAwesomeIcon icon={faPlayCircle} size="3x" style={iconStyle} />
+      { me ?
+        <Time>
+          <Count>{convertTime(hours)}</Count>
+          <Count>:</Count>
+          <Count>{convertTime(minutes)}</Count>
+          <Count>:</Count>
+          <Count>{convertTime(seconds)}</Count>
+        </Time>
+        : <Time><Count>00 : 00 : 00</Count></Time>
+      }
+      { isStop
+        ? <FontAwesomeIcon icon={faPlayCircle} size="3x" style={iconStyle} onClick={operateTimer} />
+        : <FontAwesomeIcon icon={faPauseCircle} size="3x" style={iconStyle} onClick={operateTimer} />
+      }
       <FontAwesomeIcon icon={faStopCircle} size="3x" style={iconStyle} />
     </Wrapper>
   );
 };
 
-export const SmallTimer = ({ hours, minutes, seconds, operateTimer }) => {
+export const SmallTimer = ({ hours, minutes, seconds }) => {
   return (
     <SmallWrapper>
       <Time>
--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,81 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppLayout from '../components/AppLayout';
 import { Timer } from '../components/Timer';
 import useFetch from '../hooks/useFetch';
 import ContentsTable from '../components/ContentsTable';
+import { io } from "socket.io-client";
 
 const Home = () => {
+  const [now, setNow] = useState(0);
   const [hours, setHours] = useState(12);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [isStop, setIsStop] = useState(true);
+  const [isConnected, setIsConnected] = useState(false);
   const { data: me, error, isLoading } = useFetch('/user');
   
   if (error) return <div>에러 발생</div>;
   // if (isLoading) return <div>로딩 중</div>;
 
-  // const webSocket = new WebSocket("ws://localhost:3060");
-  // webSocket.onOpen = function () {
-  //   console.log('서버와 웹소켓 연결 성공');
-  // };
-  // webSocket.onmessage = function (event) {
-  //   console.log(event.data);
-  //   webSocket.send(JSON.stringify({
-  //     hours,
-  //   }));
-  // };
+  const ENDPOINT = 'http://localhost:3060/time';
 
-  const operateTimer = (message) => () => {
-    if (message === 'start') {
-      setIsStop(false);
-    } else {
-      setIsStop(true);
+  useEffect(() => {
+    const socket = io(ENDPOINT, {
+      // cors: { origin: 'http://localhost:3060', credentials: true },
+    });
+
+    if (!isConnected) {
+      socket.on("connect", () => {
+        console.log('connected');
+      });
+      setIsConnected(true);
     }
+
+    socket.on("disconnect", () => {
+      console.log('disconnected');
+      setIsConnected(false);
+    });
+
+    console.log(isStop);
+    if (!isStop) {
+      socket.emit('start');
+      socket.on('running', (data) => {
+        console.log(data);
+        setNow(parseInt(data.time, 10));
+      });
+    } else {
+      socket.emit('stop');
+    }
+  }, [isConnected, isStop]);
+
+  useEffect(() => {
+    calculateTime(now);
+  }, [now])
+
+  const operateTimer = () => {
+    if (!me) return alert('먼저 로그인해 주세요');
+    if (isStop) return setIsStop(false);
+    return setIsStop(true);
   }
+
+  const calculateTime = (value) => {
+    let hours = parseInt(value / 3600)
+    let minutes = parseInt(value / 60) - (60 * hours);
+    let seconds = value - (3600 * hours) - (60 * minutes);
+    setHours(hours);
+    setMinutes(minutes);
+    setSeconds(seconds);
+  };
 
   return (
     <>
       <AppLayout me={me}>
         <Timer
+          me={me}
           hours={hours}
           minutes={minutes}
           seconds={seconds}
+          isStop={isStop}
           operateTimer={operateTimer}
         />
         <ContentsTable />
