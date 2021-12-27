3b6dd4a768efb533fae72be11c8947c9cba703f0
Honey <yurjune@naver.com>
2021-12-28 01:06:44 +0900

    웹소켓 제거

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import AppLayout from '../components/AppLayout';
 import { Timer } from '../components/Timer';
 import useFetch from '../hooks/useFetch';
 import ContentsTable from '../components/ContentsTable';
-import { io } from "socket.io-client";
 
 const Home = () => {
   const [now, setNow] = useState(0);
@@ -11,42 +10,10 @@ const Home = () => {
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [isStop, setIsStop] = useState(true);
-  const [isConnected, setIsConnected] = useState(false);
   const { data: me, error, isLoading } = useFetch('/user');
   
   if (error) return <div>에러 발생</div>;
   // if (isLoading) return <div>로딩 중</div>;
-
-  const ENDPOINT = 'http://localhost:3060/time';
-
-  useEffect(() => {
-    const socket = io(ENDPOINT, {
-      // cors: { origin: 'http://localhost:3060', credentials: true },
-    });
-
-    if (!isConnected) {
-      socket.on("connect", () => {
-        console.log('connected');
-      });
-      setIsConnected(true);
-    }
-
-    socket.on("disconnect", () => {
-      console.log('disconnected');
-      setIsConnected(false);
-    });
-
-    console.log(isStop);
-    if (!isStop) {
-      socket.emit('start');
-      socket.on('running', (data) => {
-        console.log(data);
-        setNow(parseInt(data.time, 10));
-      });
-    } else {
-      socket.emit('stop');
-    }
-  }, [isConnected, isStop]);
 
   useEffect(() => {
     calculateTime(now);
