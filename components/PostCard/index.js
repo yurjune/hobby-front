58126fa68c34bea388ac0409aa87919e643a6b18
Honey <yurjune@naver.com>
2022-01-03 04:09:41 +0900

    스타일 변경

--- components/PostCard/index.js
+++ components/PostCard/index.js
@@ -11,28 +11,30 @@ import { Avatar } from '../Common/custom';
 import { Flex, FlexC, Text } from '../Common';
 import { localhost } from '../Common/global';
 import { PostTimer } from '../Timer';
+import useLike from '../../hooks/useLike';
 
 const PostCard = ({ data }) => {
   const router = useRouter();
   const onClickCard = () => router.push(`/social/${data.id}`);
+
   return (
     <>{ data &&
-      <FlexC cursor="pointer" onClick={onClickCard}>
-        <Picture url={localhost(data.Images[0]?.src)} />
-        <FlexC p="10px 5px">
+      <FlexC cursor="pointer">
+        <Picture url={localhost(data.Images[0]?.src)} onClick={onClickCard} />
+        <FlexC p="10px 5px" onClick={onClickCard}>
           <Flex mb="15px">
             <Avatar mr="15px" url={localhost(data.User.Image?.src)} />
             <Text self="center" flex="1">{data.User?.name}</Text>
             <PostTimer time={data.time} />
           </Flex>
           <Paragraph>{data.content}</Paragraph>
-          <Flex>
-            <FontAwesomeIcon size="lg" icon={faHeart} style={iconStyle} />
-            <Text self="center" mr="15px">{data.Likers?.length}</Text>
-            <FontAwesomeIcon size="lg" icon={faComment} style={iconStyle} />
-            <Text self="center" mr="15px">{data.Comments?.length}</Text>
-          </Flex>
         </FlexC>
+        <Flex p="0 5px" mb="10px">
+          <FontAwesomeIcon size="lg" icon={faHeart} style={iconStyle} />
+          <Text self="center" mr="15px">{data.Likers?.length}</Text>
+          <FontAwesomeIcon size="lg" icon={faComment} style={iconStyle} />
+          <Text self="center" mr="15px">{data.Comments?.length}</Text>
+        </Flex>
       </FlexC>
     }</>
   );
