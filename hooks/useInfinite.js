30690aa14f5e36ec8e750ba9b9ae96e26c7c0898
Honey <yurjune@naver.com>
2022-01-04 00:28:48 +0900

    useInfinite 수정

--- hooks/useInfinite.js
+++ hooks/useInfinite.js
@@ -4,11 +4,13 @@ import axios from 'axios';
 axios.defaults.baseURL = 'http://localhost:3060';
 axios.defaults.withCredentials = true;
 
+export const limit = 4;
+
 const useInfinite = (address, lastId) => {
 
   const getKey = (pageIndex, previousPageData) => {
     if (previousPageData && !previousPageData.length) return null; // 끝에 도달
-    return `${address}?page=${pageIndex}&limit=4?lastId=${lastId}`;
+    return `${address}?page=${pageIndex}&limit=${limit}&lastId=${lastId}`;
   }
   const fetcher = (url) => axios.get(url).then((result) => result.data);
 
