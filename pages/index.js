f84834c22e613c11d8d297e42ca33606245cbe02
Honey <yurjune@naver.com>
2022-01-04 00:18:47 +0900

    버튼 로딩 -> 스크롤 로딩

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import useInfinite from '../hooks/useInfinite';
 import { Button } from '../components/Common/custom';
 import { Flex } from '../components/Common';
 import SearchBar from '../components/SearchBar';
+import { limit } from '../hooks/useInfinite';
 
 const Wrapper = styled.div`
   @media screen and (min-width: 768px) {
@@ -26,13 +27,23 @@ const Home = () => {
   const { data: me, error: meError, isLoading: meIsLoading } = useFetch('/user');
   const { data, error, size, setSize } = useInfinite('/posts');
 
-  useEffect(() => {
-
-  });
-
   const loadMorePosts = () => {
     setSize(size + 1);
   };
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.pageYOffset + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300) {
+        if (data[data.length - 1].length >= limit) {
+          loadMorePosts();
+        }
+      }
+    };
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, [data]);
 
   if (meError || error) return <div>에러 발생</div>;
   console.log('data:', data);
@@ -45,9 +56,9 @@ const Home = () => {
       <Wrapper>
         {data && data.map(item => item.map(post => (<PostCard key={post.id} data={post} />)))}
       </Wrapper>
-      <Flex p="10px" justify="flex-end">
+      {/* <Flex p="10px" justify="flex-end">
         <Button onClick={loadMorePosts}>더 보기</Button>
-      </Flex>
+      </Flex> */}
     </AppLayout>
   );
 };
--- pages/social/[id].js
+++ pages/social/[id].js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import AppLayout from '../../components/AppLayout';
 import PostCardBig from '../../components/PostCardBig';
@@ -6,6 +6,7 @@ import useFetch from '../../hooks/useFetch';
 import useInfinite from '../../hooks/useInfinite';
 import { Flex } from '../../components/Common';
 import { Button } from '../../components/Common/custom';
+import { limit } from '../../hooks/useInfinite';
 
 const Social = () => {
   const router = useRouter();
@@ -17,6 +18,20 @@ const Social = () => {
     setSize(size + 1);
   };
 
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.pageYOffset + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300) {
+        if (data[data.length - 1].length >= limit) {
+          loadMorePosts();
+        }
+      }
+    };
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, [data]);
+
   if (error || meError) return <div>에러 발생</div>;
   if (!router.isReady) return <div>로딩중...</div>;
 
@@ -25,9 +40,9 @@ const Social = () => {
       {data && data.map(item => item.map(post => (
         <PostCardBig key={post.id} me={me} postData={post} mutate={mutate} />
       )))}
-      <Flex maxW="650px" m="0 auto" p="10px" justify="flex-end">
+      {/* <Flex maxW="650px" m="0 auto" p="10px" justify="flex-end">
         <Button onClick={loadMorePosts}>더 보기</Button>
-      </Flex>
+      </Flex> */}
     </AppLayout>
   );
 };
